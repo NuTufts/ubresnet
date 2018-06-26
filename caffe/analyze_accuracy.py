9714d5da35cae5e5267dde96e954f9ed94db91b4
Taritree <taritree.wongjirad@gmail.com>
2018-06-26 01:47:34 -0400

    analyze stub

---
+++ caffe/analyze_accuracy.py
@@ -0,0 +1,5 @@
+import os,sys
+
+# Takes input and output of SSNet. Input is assumed to have true labels.
+# Calculates basic accuracy metrics
+# To-do: expand to include designation of ambiguous label. This is if shower is on trunk of cosmic muon.
