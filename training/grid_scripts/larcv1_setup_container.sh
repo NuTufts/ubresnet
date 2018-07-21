9f346197c5ef832c2a9456a90e0e519ea2467d8b
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2018-07-21 19:11:44 +0000

    fixed setup container script

--- training/grid_scripts/larcv1_setup_container.sh
+++ training/grid_scripts/larcv1_setup_container.sh
@@ -6,16 +6,30 @@ repodir_in_container=$1
 startdir=$PWD
 
 # setup CUDA
-export PATH=/usr/local/nvidia:${PATH}
-export LD_LIBRARY_PATH=/usr/local/nvidia:${LD_LIBRARY_PATH}
+#export PATH=/usr/local/nvidia:${PATH}
+#export LD_LIBRARY_PATH=/usr/local/nvidia:${LD_LIBRARY_PATH}
 
 # setup ROOT
 source /usr/local/root/release/bin/thisroot.sh
 
 # go to repo dir
-cd $repodir
+cd ${repodir_in_container}
 
-source larcv1_configure.sh
+export UBRESNET_BASEDIR=$PWD
+export UBRESNET_MODELDIR=${UBRESNET_BASEDIR}/models
+export LARCV_VERSION=1
+
+# setup larlite environment variables
+cd larlite
+source config/setup.sh
+
+# setup larcv environment variabls
+cd ../caffe/larcv
+source configure.sh
+
+# add larcvdataset folder to pythonpath
+cd ../../larcvdataset
+source setenv.sh
 
 # go back to startdir
 cd $startdir
