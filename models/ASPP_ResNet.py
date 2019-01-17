3491bf2305d95b252cc2499850ea853212536c54
kseuro <kai.kharpertian@gmail.com>
2019-01-17 12:27:59 -0500

    cleaned up model script and removed redundant file

--- models/ASPP_ResNet.py
+++ models/ASPP_ResNet.py
@@ -1,17 +1,22 @@
+# U-Net with Residual Skip Connections
+# Three Atrous Spatial Pyramid Pooling Layers
+#
+# Tufts University -- Department of Physics
+# High Energy Physics Research Group
 # Liquid Argon Computer Vision
-# Atrous Spatial Pyramid Pooling Architecture (ASPP)
-# U-Net with ResNet modules
-
-# e_4 & e_5 skip connections with image pooling
-
-# Import Script
+#
+##########################################
+# Import Scripts
+##########################################
 import torch.nn as nn
 import torch as torch
 import math
 import torch.utils.model_zoo as model_zoo
 from numbers import Integral
 
-# python,numpy
+##########################################
+# Python, Numpy
+##########################################
 import os
 import sys
 import commands
@@ -20,11 +25,15 @@ import time
 import traceback
 import numpy as np
 
-# ROOT, larcv
+##########################################
+# ROOT, LArCV
+##########################################
 import ROOT
 from larcv import larcv
 
-# torch
+##########################################
+# Torch
+##########################################
 import torch
 import torch.nn as nn
 import torch.nn.parallel
@@ -38,13 +47,17 @@ import torchvision.datasets as datasets
 import torchvision.models as models
 import torch.nn.functional as F
 import warnings
+##########################################
 
-#####
+##########################################
+# Function Definitions
+##########################################
 
 
 def conv3x3(in_planes, out_planes, stride=1):
     """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
 
 
 class BasicBlock(nn.Module):
@@ -63,7 +76,8 @@ class BasicBlock(nn.Module):
         self.bypass = None
         self.bnpass = None
         if inplanes != planes or stride > 1:
-            self.bypass = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, padding=0, bias=False)
+            self.bypass = nn.Conv2d(inplanes, planes, kernel_size=1,
+                                    stride=stride, padding=0, bias=False)
             self.bnpass = nn.BatchNorm2d(planes)
 
         self.relu = nn.ReLU(inplace=True)
@@ -99,7 +113,8 @@ class Bottleneck(nn.Module):
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
 
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.conv3 = nn.Conv2d(planes, planes, kernel_size=1, bias=False)
@@ -110,7 +125,8 @@ class Bottleneck(nn.Module):
 
         # if stride >1, then we need to subsamble the input
         if stride > 1:
-            self.shortcut = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False)
+            self.shortcut = nn.Conv2d(inplanes, planes, kernel_size=1,
+                                      stride=stride, bias=False)
         else:
             self.shortcut = None
 
@@ -155,8 +171,11 @@ class DoubleResNet(nn.Module):
 class ConvTransposeLayer(nn.Module):
     def __init__(self, deconv_inplanes, deconv_outplanes, res_outplanes):
         super(ConvTransposeLayer, self).__init__()
-        self.deconv = nn.ConvTranspose2d(deconv_inplanes, deconv_outplanes, kernel_size=4, stride=2, padding=1, bias=False)
-        self.res = DoubleResNet(res_outplanes + deconv_outplanes, res_outplanes, stride=1)
+        self.deconv = nn.ConvTranspose2d(deconv_inplanes, deconv_outplanes,
+                                         kernel_size=4, stride=2, padding=1,
+                                         bias=False)
+        self.res = DoubleResNet(res_outplanes + deconv_outplanes, res_outplanes,
+                                stride=1)
 
     def forward(self, x, skip_x):
         out = self.deconv(x, output_size=skip_x.size())
@@ -167,36 +186,43 @@ class ConvTransposeLayer(nn.Module):
 
 
 class ASPP(nn.Module):
-    def __init__(self, inplanes, outplanes=16, nkernels=16):
+    def __init__(self, inplanes, outplanes=16, nkernels=16, showsizes=False):
         super(ASPP, self).__init__()
 
         stride = 1
         self.inplanes = inplanes
         self.outplanes = outplanes
         self.nkernels = nkernels
+        self.showsizes = showsizes
 
         # Block 1
-        self.B1_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=1, stride=stride, padding=0, dilation=1, bias=True)
+        self.B1_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=1,
+                                 stride=stride, padding=0, dilation=1, bias=True)
         self.B1_bn = nn.BatchNorm2d(self.nkernels)
         self.B1_relu = nn.ReLU(inplace=True)
 
         # Block 2
-        self.B2_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=3, stride=stride, padding=1, dilation=1, bias=True)
+        self.B2_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=3,
+                                 stride=stride, padding=1, dilation=1, bias=True)
         self.B2_bn = nn.BatchNorm2d(self.nkernels)
         self.B2_relu = nn.ReLU(inplace=True)
 
         # Block 3
-        self.B3_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=3, stride=stride, padding=3, dilation=3, bias=True)
+        self.B3_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=3,
+                                 stride=stride, padding=3, dilation=3, bias=True)
         self.B3_bn = nn.BatchNorm2d(self.nkernels)
         self.B3_relu = nn.ReLU(inplace=True)
 
         # Block 4
-        self.B4_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=3, stride=stride, padding=5, dilation=5, bias=True)
+        self.B4_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=3,
+                                 stride=stride, padding=5, dilation=5, bias=True)
         self.B4_bn = nn.BatchNorm2d(self.nkernels)
         self.B4_relu = nn.ReLU(inplace=True)
 
         # Block 5
-        self.B5_gp = torch.nn.MaxPool2d(kernel_size=3, stride=stride, padding=1, dilation=1, return_indices=False, ceil_mode=False)
+        self.B5_gp = torch.nn.MaxPool2d(kernel_size=3, stride=stride, padding=1,
+                                        dilation=1, return_indices=False,
+                                        ceil_mode=False)
 
     def forward(self, x):
 
@@ -224,6 +250,13 @@ class ASPP(nn.Module):
         # Block 5
         b5 = self.B5_gp(x)
 
+        if self.showsizes:
+            print "b1 dim:", b1.size()
+            print "b2 dim:", b2.size()
+            print "b3 dim:", b3.size()
+            print "b4 dim:", b4.size()
+            print "b5 dim:", b5.size()
+
         # Concatenation along the depth
         x = torch.cat((b1, b2, b3, b4, b5), 1)
 
@@ -239,7 +272,8 @@ class ASPP_post(nn.Module):  # from ASPP_combine
         self.nkernels = outplanes
         self.outplanes = outplanes
 
-        self.ASPP_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=1, stride=1, padding=0, bias=True)
+        self.ASPP_conv = nn.Conv2d(self.inplanes, self.outplanes, kernel_size=1,
+                                   stride=1, padding=0, bias=True)
         self.ASPP_bn = nn.BatchNorm2d(self.nkernels)
         self.ASPP_relu = nn.ReLU(inplace=True)
 
@@ -254,11 +288,13 @@ class ASPP_post(nn.Module):  # from ASPP_combine
 
 class ASPP_ResNet(nn.Module):
 
-    def __init__(self, num_classes=3, in_channels=3, inplanes=16, showsizes=False):
+    def __init__(self, num_classes=3, in_channels=3, inplanes=16, showsizes=True):
         self.inplanes = inplanes
         super(ASPP_ResNet, self).__init__()
 
+        ######################################################################
         # Class variables
+        ######################################################################
         stride = 1
         stride_mp = 2
 
@@ -268,49 +304,93 @@ class ASPP_ResNet(nn.Module):
         self.num_classes = num_classes
         self.showsizes = showsizes
 
-        # Encoder
+        ######################################################################
+        ############################## Encoder ###############################
+        ######################################################################
 
-        # Stem 1
-        self.conv1 = nn.Conv2d(in_channels, self.inplanes, kernel_size=7, stride=1, padding=3, bias=True)  # initial conv layer
+        ######################################################################
+        # Stem 1 & initial convolution layer
+        ######################################################################
+        self.conv1 = nn.Conv2d(in_channels, self.inplanes, kernel_size=7,
+                               stride=1, padding=3, bias=True)
         self.bn1 = nn.BatchNorm2d(self.inplanes)
         self.relu1 = nn.ReLU(inplace=True)
         self.pool1 = nn.MaxPool2d(3, stride=2, padding=1)
 
-        # Encoding Layer
-        self.enc_layer1 = self._make_encoding_layer(self.inplanes * 1, self.inplanes * 2, stride=1)  # 16->32
-        self.enc_layer2 = self._make_encoding_layer(self.inplanes * 2, self.inplanes * 4, stride=2)  # 32->64
-        self.enc_layer3 = self._make_encoding_layer(self.inplanes * 4, self.inplanes * 8, stride=2)  # 64->128
-        self.enc_layer4 = self._make_encoding_layer(self.inplanes * 8, self.inplanes * 16, stride=2)  # 128->256
-        self.enc_layer5 = self._make_encoding_layer(self.inplanes * 16, self.inplanes * 32, stride=2)  # 256->512
+        ######################################################################
+        # Encoding Layer (16->32, 32->64, 64->128, 128->256, 256->512)
+        ######################################################################
+        self.enc_layer1 = self._make_encoding_layer(self.inplanes * 1,
+                                                    self.inplanes * 2,
+                                                    stride=1)
+        self.enc_layer2 = self._make_encoding_layer(self.inplanes * 2,
+                                                    self.inplanes * 4,
+                                                    stride=2)
+        self.enc_layer3 = self._make_encoding_layer(self.inplanes * 4,
+                                                    self.inplanes * 8,
+                                                    stride=2)
+        self.enc_layer4 = self._make_encoding_layer(self.inplanes * 8,
+                                                    self.inplanes * 16,
+                                                    stride=2)
+        self.enc_layer5 = self._make_encoding_layer(self.inplanes * 16,
+                                                    self.inplanes * 32,
+                                                    stride=2)
 
+        ######################################################################
         # Atrous Spatial Pyramid Pooling (ASPP)
-
+        ######################################################################
         self.ASPP_layer_enc3 = self.ASPP_layer(self.inplanes * 8)
-        self.ASPP_combine_enc3 = self.ASPP_combine(self.inplanes * 4, self.inplanes * 8)
+        self.ASPP_combine_enc3 = self.ASPP_combine(self.inplanes * 12,
+                                                   self.inplanes * 8)
 
         self.ASPP_layer_enc4 = self.ASPP_layer(self.inplanes * 16)
-        self.ASPP_combine_enc4 = self.ASPP_combine(self.inplanes * 20, self.inplanes * 16)
+        self.ASPP_combine_enc4 = self.ASPP_combine(self.inplanes * 20,
+                                                   self.inplanes * 16)
 
         self.ASPP_layer_enc5 = self.ASPP_layer(self.inplanes * 32)
-        self.ASPP_combine_enc5 = self.ASPP_combine(self.inplanes * 36, self.inplanes * 32)
+        self.ASPP_combine_enc5 = self.ASPP_combine(self.inplanes * 36,
+                                                   self.inplanes * 32)
 
-        # Decoding Layer
-        self.dec_layer5 = self._make_decoding_layer(self.inplanes * 64, self.inplanes * 16, self.inplanes * 32)  # 512->256
-        self.dec_layer4 = self._make_decoding_layer(self.inplanes * 32, self.inplanes * 8, self.inplanes * 8)  # 256->128
-        self.dec_layer3 = self._make_decoding_layer(self.inplanes * 8, self.inplanes * 4, self.inplanes * 4)  # 128->64
-        self.dec_layer2 = self._make_decoding_layer(self.inplanes * 4, self.inplanes * 2, self.inplanes * 2)  # 64->32
-        self.dec_layer1 = self._make_decoding_layer(self.inplanes * 2, self.inplanes, self.inplanes)  # 32->16
+        ######################################################################
+        ############################## Decoder ###############################
+        ######################################################################
 
+        ######################################################################
+        # Decoding Layer (512->256, 256->128, 128->64, 64->32, 32->16)
+        ######################################################################
+        self.dec_layer5 = self._make_decoding_layer(self.inplanes * 64,
+                                                    self.inplanes * 16,
+                                                    self.inplanes * 32)
+        self.dec_layer4 = self._make_decoding_layer(self.inplanes * 32,
+                                                    self.inplanes * 8,
+                                                    self.inplanes * 16)
+        self.dec_layer3 = self._make_decoding_layer(self.inplanes * 16,
+                                                    self.inplanes * 4,
+                                                    self.inplanes * 4)
+        self.dec_layer2 = self._make_decoding_layer(self.inplanes * 4,
+                                                    self.inplanes * 2,
+                                                    self.inplanes * 2)
+        self.dec_layer1 = self._make_decoding_layer(self.inplanes * 2,
+                                                    self.inplanes,
+                                                    self.inplanes)
+
+        ######################################################################
         # Final conv stem (7x7) = (3x3)^3
+        ######################################################################
         self.nkernels = 16
-        self.conv10 = nn.Conv2d(self.inplanes, self.nkernels, kernel_size=7, stride=1, padding=3, bias=True)
+        self.conv10 = nn.Conv2d(self.inplanes, self.nkernels, kernel_size=7,
+                                stride=1, padding=3, bias=True)
         self.bn10 = nn.BatchNorm2d(self.nkernels)
         self.relu10 = nn.ReLU(inplace=True)
 
-        self.conv11 = nn.Conv2d(self.inplanes, num_classes, kernel_size=7, stride=1, padding=3, bias=True)
+        self.conv11 = nn.Conv2d(self.inplanes, num_classes, kernel_size=7,
+                                stride=1, padding=3, bias=True)
 
-        # we use log softmax in order to more easily pair it with
-        self.softmax = nn.LogSoftmax(dim=1)  # should return [b,c=3,h,w], normalized over, c dimension
+        ######################################################################
+        # Loss Function: Log softmax
+        # Should return [b,c=3,h,w], normalized over c dimension
+        ######################################################################
+        self.softmax = nn.LogSoftmax(dim=1)
 
         # initialization
         for m in self.modules():
@@ -338,7 +418,9 @@ class ASPP_ResNet(nn.Module):
         if self.showsizes:
             print "input: ", x.size(), " is_cuda=", x.is_cuda
 
-        # stem
+        ######################################################################
+        # Stem 1 & initial convolution layer
+        ######################################################################
         x = self.conv1(x)
         x = self.bn1(x)
         x0 = self.relu1(x)
@@ -347,7 +429,9 @@ class ASPP_ResNet(nn.Module):
         if self.showsizes:
             print "after conv1, x0: ", x0.size()
 
-        # Encoding Layer
+        ######################################################################
+        # Encoding Layer (16->32, 32->64, 32->64, 64->128, 128->256, 256->512)
+        ######################################################################
         e1 = self.enc_layer1(x)
         e2 = self.enc_layer2(e1)
         e3 = self.enc_layer3(e2)
@@ -361,11 +445,20 @@ class ASPP_ResNet(nn.Module):
             print "  e4: ", e4.size()
             print "  e5: ", e5.size()
 
-        # ASPP
+        ######################################################################
+        # ASPP & Concatenated Skip Connections
+        ######################################################################
+        e3_ASPP = self.ASPP_layer_enc3(e3)
+        if self.showsizes:
+            print "e3_ASPP size after ASPP_layer:", e3_ASPP.size()
 
-        # e3_ASPP = self.ASPP_layer_enc3(e3)
-        # e3_ASPP = self.ASPP_combine_enc3(e3_ASPP)
-        # e3_skip = torch.cat((e3_ASPP, e3), 1)
+        e3_ASPP = self.ASPP_combine_enc3(e3_ASPP)
+        if self.showsizes:
+            print "e3_ASPP size after ASPP_combine:", e3_ASPP.size()
+
+        e3_skip = torch.cat((e3_ASPP, e3), 1)
+        if self.showsizes:
+            print "e4_skip size after cat:", e3_skip.size()
 
         e4_ASPP = self.ASPP_layer_enc4(e4)
         if self.showsizes:
@@ -391,10 +484,12 @@ class ASPP_ResNet(nn.Module):
         if self.showsizes:
             print "e5_skip size after cat:", e5_skip.size()
 
-        # Decoding Layer
+        ######################################################################
+        # Decoding Layer (512->256, 256->128, 128->64, 64->32, 32->16)
+        ######################################################################
         d5 = self.dec_layer5(e5_skip, e4_skip)
 
-        d4 = self.dec_layer4(d5, e3)
+        d4 = self.dec_layer4(d5, e3_skip)
         if self.showsizes:
             print "  dec4: ", d4.size(), " iscuda=", d4.is_cuda
 
@@ -410,13 +505,17 @@ class ASPP_ResNet(nn.Module):
 
         if self.showsizes:
             print "  dec1: ", d1.size(), " iscuda=", d1.is_cuda
-
+        ######################################################################
+        # Final conv stem (7x7) = (3x3)^3
+        ######################################################################
         x = self.conv10(d1)
         x = self.bn10(x)
         x = self.relu10(x)
-
         x = self.conv11(x)
 
+        ######################################################################
+        # Loss Function
+        ######################################################################
         x = self.softmax(x)
         if self.showsizes:
             print "  softmax: ", x.size()
